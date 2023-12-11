7ed10a37e5c853e40d55e40cf44215a485e87743
maxprodigy <peterphilipjohnson@gmail.com>
2023-12-11 20:02:29 +0000

    Added test.py

---
+++ test.py
@@ -0,0 +1,9 @@
+from nevents_translate import NeventsTranslate
+
+# create an object of the class NevetsTranslate
+test = NeventsTranslate()
+test.translate(
+    from_lang_text=
+    "My Name isChidera Asaph.\nI am a student of African Leadership University(A.L.U)\nI love Playing Football and I am a fan of Chelsea FC.",
+    from_lang="english",
+    to_lang="yoruba")
